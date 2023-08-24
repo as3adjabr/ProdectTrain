386de2aef3969e33f43ccb79de60456e14d5c799
acer <acer@DESKTOP-O51RO0J>
2023-08-24 18:49:32 +0300

    test

---
+++ Project1/Project1/Data/Migrations/20230824154850_add_get_all1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project1.Data.Migrations
+{
+    public partial class add_get_all1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Project1/Project1/Models/Product.cs
+++ Project1/Project1/Models/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace Project1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
 {
     public class Product
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public float price { get; set; }
